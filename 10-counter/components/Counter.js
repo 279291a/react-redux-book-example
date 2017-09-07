3bf4384abc43624913fc8b07819697a655bb7d18
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-07 15:26:50 +0800

    添加组件方法

--- 10-counter/components/Counter.js
+++ 10-counter/components/Counter.js
@@ -3,6 +3,18 @@ import React, { PropTypes } from 'react';
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
+    this.incrementIfOdd = this.incrementIfOdd.bind(this);
+    this.incrementAsync = this.incrementAsync.bind(this);
+  }
+
+  incrementIfOdd() {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement();
+    }
+  }
+
+  incrementAsync() {
+    setTimeout(this.props.onIncrement, 100);
   }
 
   render() {
