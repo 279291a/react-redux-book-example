28d9f474089c5c2bc97d9dcf639e5330b70a798e
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-07 15:24:04 +0800

    组件的render方法

--- 10-counter/components/Counter.js
+++ 10-counter/components/Counter.js
@@ -1,7 +1,30 @@
-import { PropTypes } from 'react';
+import React, { PropTypes } from 'react';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { value, onDecrement, onIncrement } = this.props;
+    return (
+      <p>clicked:
+			{value}times
+			{'  '}
+        <button onClick={onIncrement}>+</button>
+        {'  '}
+        <button onClick={onDecrement}>-</button>
+        {'  '}
+        <button onClick={this.incrementIfOdd}>increment If Odd</button>
+        {'  '}
+        <button onClick={this.incrementAsync}>increment async</button>
+      </p>
+    );
+  }
+}
 
 Counter.propTypes = {
-	value: PropTypes.number.isRequired,
-	onIncrement:PropTypes.func.isRequired,
-	onDecrement:PropTypes.func.isRequired,
-}
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+};
