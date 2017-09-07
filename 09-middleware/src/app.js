f0f139c833e8df192bb3856699e62c0afeebe989
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-07 14:43:02 +0800

    action创建函数

--- 09-middleware/src/app.js
+++ 09-middleware/src/app.js
@@ -1,5 +1,32 @@
 import { createStore } from 'redux';
 
+function increment() {
+  return { type: 'INCREMENT' };
+}
+
+function decrement() {
+  return { type: 'DECREMENT' };
+}
+
+function incrementIfOdd() {
+  return (dispatch, getState) => {
+    const value = getState();
+    if (value % 2 === 0) {
+      return;
+    }
+    dispatch(increment());
+  };
+}
+
+function incrementAsync(delay = 1000) {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(increment());
+    }, delay);
+  };
+}
+
+
 function counter(state = 0, action) {
   switch (action.type) {
     case 'INCREMENT':
