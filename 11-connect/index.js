fd8b3c1343321ae6ae0b566ebfa9d7c422e084cc
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-08 13:33:44 +0800

    connect第二种连接方式

--- 11-connect/index.js
+++ 11-connect/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import counter from './reducers';
 import Connect1 from './containers/Connect1';
+import Connect2 from './containers/Connect2';
 
 const store = createStore(counter, applyMiddleware(thunk));
 
@@ -16,6 +17,10 @@ ReactDOM.render(
         <li>
           connect()的前两个参数分别为函数和对象：
           <Connect1 />
+        </li>
+        <li>
+          connect()的前两个参数分别为函数和dispatch函数：
+          <Connect2 />
         </li>
       </ul>
     </div>
