f6b17d4d98caa2b74ad80344e1548abf84d3a7f0
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-08 18:31:53 +0800

    测试action 创建函数

---
+++ 13-test/test/actions/counter.spec.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import * as actions from '../../actions';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+describe('actions', () => {
+  describe('counter'), () => {
+    it('increment should create increment action', () => {
+      const expectedActions = [{ type: actions.INCREMENT }];
+      const store = mockStore({ counter: 1 });
+      store.dispatch(actions.incrementIfOdd());
+      expect(store.getActions().toEqual(expectedActions));
+    });
+
+    it('decrement should create decrement action', () => {
+      expect(actions.DECREMENT()).toEqual({ type: actions.DECREMENT });
+    });
+  };
+});
