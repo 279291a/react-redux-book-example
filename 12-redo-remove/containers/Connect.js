cc92972d1835fe2d2e7c74baa2df294c926452ed
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-08 15:51:06 +0800

    修改value 为当前值

--- 12-redo-remove/containers/Connect.js
+++ 12-redo-remove/containers/Connect.js
@@ -3,6 +3,6 @@ import Counter from '../components/Counter';
 import * as ActionCreators from '../actions';
 
 export default connect(
-  state => ({ value: state.counter }),
+  state => ({ value: state.counter.present }),
   ActionCreators,
 )(Counter);
