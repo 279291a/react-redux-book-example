6d125012d5669942db5effaee74e50a7ba654b50
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-11 09:57:56 +0800

    测试action创建函数

--- 13-test/actions/index.js
+++ 13-test/actions/index.js
@@ -7,3 +7,14 @@ export const onIncrement = () => ({ type: INCREMENT });
 export const onDecrement = () => ({ type: DECREMENT });
 export const undo = () => ({ type: UNDO });
 export const redo = () => ({ type: REDO });
+export const incrementIfOdd = () => ({ dispatch, getState }) => {
+  const { counter } = getState();
+  if (counter % 2 === 0) {
+    return;
+  }
+  dispatch(onIncrement());
+};
+
+export const incrementAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(onIncrement), 1000);
+};
