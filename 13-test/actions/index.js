4a18d85747f1f2ab30b5597587dfacd448bfe8d3
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-11 13:31:11 +0800

    更改箭头函数为普通函数

--- 13-test/actions/index.js
+++ 13-test/actions/index.js
@@ -7,14 +7,22 @@ export const onIncrement = () => ({ type: INCREMENT });
 export const onDecrement = () => ({ type: DECREMENT });
 export const undo = () => ({ type: UNDO });
 export const redo = () => ({ type: REDO });
-export const incrementIfOdd = () => ({ dispatch, getState }) => {
-  const { counter } = getState();
-  if (counter % 2 === 0) {
-    return;
-  }
-  dispatch(onIncrement());
-};
+export function incrementIfOdd() {
+  return (dispatch, getState) => {
+    const { counter } = getState();
 
-export const incrementAsync = () => (dispatch) => {
-  setTimeout(() => dispatch(onIncrement), 1000);
-};
+    if (counter % 2 === 0) {
+      return;
+    }
+
+    dispatch(onIncrement());
+  };
+}
+
+export function incrementAsync() {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(onIncrement());
+    }, 1000);
+  };
+}
