007e460f9ac34362f45b5a8e6df8a17b694a4cf4
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-11 14:28:57 +0800

    修正 测试app 代码

--- 13-test/test/containers/App.spec.js
+++ 13-test/test/containers/App.spec.js
@@ -1,14 +1,14 @@
 import expect from 'expect';
 import React from 'react';
 import { mount } from 'enzyme';
-import { Provider } from 'redux';
+import { Provider } from 'react-redux';
 import App from '../../containers/Connect';
 import configureStore from '../../store/configureStore';
 
 function setup(initialState) {
   const store = configureStore(initialState);
   const app = mount(
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>,
   );
@@ -24,31 +24,31 @@ describe('containers', () => {
   describe('App', () => {
     it('should display initial count', () => {
       const { p } = setup();
-      expect(p.text()).toMatch(/^clicked: 0 times/);
+      expect(p.text()).toMatch(/^clicked:0times/);
     });
 
     it('should display updated counter after increment button click', () => {
       const { buttons, p } = setup();
       buttons.at(0).simulate('click');
-      expect(p.text()).toMatch(/^clicked: 1 times/);
+      expect(p.text()).toMatch(/^clicked:1times/);
     });
 
     it('should display updated counter after decrement button click', () => {
       const { buttons, p } = setup();
       buttons.at(1).simulate('click');
-      expect(p.text()).toMatch(/^clicked: -1 times/);
+      expect(p.text()).toMatch(/^clicked:-1times/);
     });
 
     it('shouldnt change if even and odd button clicked', () => {
       const { buttons, p } = setup();
       buttons.at(2).simulate('click');
-      expect(p.text()).toMatch(/^clicked: 0 times/);
+      expect(p.text()).toMatch(/^clicked:0times/);
     });
 
     it('should change if even and odd button clicked', () => {
       const { buttons, p } = setup({ counter: 1 });
       buttons.at(2).simulate('click');
-      expect(p.text()).toMatch(/^clicked: 2 times/);
+      expect(p.text()).toMatch(/^clicked:2times/);
     });
   });
 });
