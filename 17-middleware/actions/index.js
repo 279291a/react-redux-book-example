4fd2d4fb0e9511b07a692d192b4a45c6ca02b2a6
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-13 17:01:18 +0800

    action

--- 17-middleware/actions/index.js
+++ 17-middleware/actions/index.js
@@ -1,3 +1,9 @@
+import { CALL_API, Schemas } from '../middleware/api';
+
+export const USER_REQUEST = 'USER_REQUEST';
+export const USER_SUCCESS = 'USER_SUCCESS';
+export const USER_FAILURE = 'USER_FAILURE';
+
 function fetchUser(login) {
   return {
     [CALL_API]: {
@@ -5,5 +11,109 @@ function fetchUser(login) {
       endpoint: `users/${login}`,
       schema: Schemas.USER,
     },
+  };
+}
+
+export function loadUser(login, requiredFields = []) {
+  return (dispatch, getState) => {
+    const user = getState().entities.users[login];
+    if (user && requiredFields.every(key => user.hasOwnProperty(key))) {
+      return null;
+    }
+
+    return dispatch(fetchUser(login));
+  };
+}
+
+export const REPO_REQUEST = 'REPO_REQUEST';
+export const REPO_SUCCESS = 'REPO_SUCCESS';
+export const REPO_FAILURE = 'REPO_FAILURE';
+
+function fetchRepo(fullName) {
+  return {
+    [CALL_API]: {
+      types: [REPO_REQUEST, REPO_SUCCESS, REPO_FAILURE],
+      endpoint: `repos/${fullName}`,
+      schema: Schemas.REPO,
+    },
+  };
+}
+
+export function loadRepo(fullName, requiredFields = []) {
+  return (dispatch, getState) => {
+    const repo = getState().entities.repos[fullName];
+    if (repo && requiredFields.every(key => repo.hasOwnProperty(key))) {
+      return null;
+    }
+
+    return dispatch(fetchRepo(fullName));
+  };
+}
+
+export const STARRED_REQUEST = 'STARRED_REQUEST';
+export const STARRED_SUCCESS = 'STARRED_SUCCESS';
+export const STARRED_FAILURE = 'STARRED_FAILURE';
+
+function fetchStarred(login, nextPageUrl) {
+  return {
+    login,
+    [CALL_API]: {
+      types: [STARRED_REQUEST, STARRED_SUCCESS, STARRED_FAILURE],
+      endpoint: nextPageUrl,
+      schema: Schemas.REPO_ARRAY,
+    },
+  };
+}
+
+export function loadStarred(login, nextPage) {
+  return (dispatch, getState) => {
+    const {
+      nextPageUrl = `users/${login}/starred`,
+      pageCount = 0,
+    } = getState().pagination.starredByUsers[login] || {};
+
+    if (pageCount > 0 && !nextPage) {
+      return null;
+    }
+
+    return dispatch(fetchStarred(login, nextPageUrl));
+  };
+}
+
+export const STARGAZERS_REQUEST = 'STARGAZERS_REQUEST';
+export const STARGAZERS_SUCCESS = 'STARGAZERS_SUCCESS';
+export const STARGAZERS_FAILURE = 'STARGAZERS_FAILURE';
+
+function fetchStargazers(fullName, nextPageUrl) {
+  return {
+    fullName,
+    [CALL_API]: {
+      types: [STARGAZERS_REQUEST, STARGAZERS_SUCCESS, STARGAZERS_FAILURE],
+      endpoint: nextPageUrl,
+      schema: Schemas.USER_ARRAY,
+    },
+  };
+}
+
+export function loadStargazers(fullName, nextPage) {
+  return (dispatch, getState) => {
+    const {
+      nextPageUrl = `repo/${fullName}/stargazers`,
+      pageCount = 0,
+    } = getState().pagination.stargazersByRepo[fullName] || {};
+
+    if (pageCount > 0 && !nextPage) {
+      return null;
+    }
+
+    return dispatch(fetchStargazers(fullName, nextPageUrl));
+  };
+}
+
+export const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE';
+
+export function resetErrorMessage() {
+  return {
+    type: RESET_ERROR_MESSAGE,
   };
 }
