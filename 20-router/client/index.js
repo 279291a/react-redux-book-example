8a47dcb95e220acfb37dc31fae68ccd515174532
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-07 15:53:13 +0800

    渲染路由组件

--- 20-router/client/index.js
+++ 20-router/client/index.js
@@ -2,16 +2,22 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+
 import configureStore from '../common/store/configureStore';
-import App from '../common/containers/App';
+import getRoutes from '../common/routes';
 
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState);
 const root = document.getElementById('root');
+const routes = getRoutes();
+const history = browserHistory;
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      {routes}
+    </Router>
   </Provider>,
   root,
 );
