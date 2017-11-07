cd55ee9cb43dbf09d31df2ffa2d52d9314e8fc6b
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-07 13:30:21 +0800

    更改拼写错误

--- 19-3/server/index.js
+++ 19-3/server/index.js
@@ -4,9 +4,8 @@ require('babel-register');
 var path = require('path');
 var rootDir = path.resolve(__dirname,'..');
 
-var WebpackIsomorphicTools = require('webpack-isomophric-tools');
-global.webpackIsomorphicTools = new WebpackIsomorphicTools(
-  require('../wepack-isomophic-tools'))
+var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
+global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('../webpack-isomorphic-tools'))
   .development()
   .server(rootDir,function(){
     require('./server')
