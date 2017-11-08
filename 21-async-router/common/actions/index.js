4454bd44604cb25769b1acb453acf44435744fee
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-08 11:47:01 +0800

    添加redux-amrc依赖

--- 21-async-router/common/actions/index.js
+++ 21-async-router/common/actions/index.js
@@ -1,3 +1,6 @@
+import 'isomorphic-fetch';
+import { ASYNC } from 'redux-amrc';
+
 export const SET_COUNTER = 'SET_COUNTER';
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
@@ -19,3 +22,18 @@ export const incrementAsync = (delay = 1000) => (dispatch) => {
     dispatch(increment());
   }, delay);
 };
+
+export function load() {
+  return {
+    [ASYNC]: {
+      key: 'counter',
+      promise: () => fetch('http://localhost:3002/api/counter')
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+        }),
+    },
+  };
+}
