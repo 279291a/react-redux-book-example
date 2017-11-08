a796c19abba476b6a9f2fff9a7e63d54fc0a7601
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-08 12:00:26 +0800

    路由组件onEnter方法

--- 21-async-router/common/routes.js
+++ 21-async-router/common/routes.js
@@ -3,10 +3,20 @@ import { Route, IndexRoute } from 'react-router';
 import Counter from './containers/Counter';
 import Main from './containers/Main';
 import Home from './containers/Home';
+import { load } from '../common/actions';
 
-export default () => (
-  <Route path="/" component={Main}>
-    <IndexRoute component={Home} />
-    <Route path="counter" component={Counter} />
-  </Route>
-);
+export default (store) => {
+  const preload = (nextState, replace, cb) => {
+    if (__SERVER__ || nextState.location.action === 'PUSH') {
+      store.dispatch(load()).then(() => cb());
+    } else {
+      cb();
+    }
+  };
+  return (
+    <Route path="/" component={Main}>
+      <IndexRoute component={Home} />
+      <Route path="counter" component={Counter} onEnter={preload} />
+    </Route>
+  );
+};
