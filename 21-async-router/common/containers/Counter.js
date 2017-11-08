d4da5a1b2d351424655cdc631336b91621727e09
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-08 13:30:19 +0800

    装饰器写法改写Counter 组件，添加load方法

--- 21-async-router/common/containers/Counter.js
+++ 21-async-router/common/containers/Counter.js
@@ -1,40 +1,47 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as CounterActions from '../actions';
 
-function Counter({ increment, incrementAsync, decrement, incrementIfOdd, counter }) {
-  return (
-    <p>
-      clicked {counter} times:
-      {'  '}
-      <button onClick={increment}>+</button>
-      {'  '}
-      <button onClick={decrement}>-</button>
-      {'  '}
-      <button onClick={incrementIfOdd}>increment if odd</button>
-      {'  '}
-      <button onClick={() => incrementAsync()}>increment Async</button>
-    </p>
-  );
-}
+@connect(
+  state => ({ state }),
+  actionCreators
+)
 
-Counter.propTypes = {
-  increment: PropTypes.func.isRequired,
-  decrement: PropTypes.func.isRequired,
-  incrementIfOdd: PropTypes.func.isRequired,
-  incrementAsync: PropTypes.func.isRequired,
-  counter: PropTypes.number.isRequired,
-};
-
-function mapStateToProps(state) {
-  return {
-    counter: state.counter,
+class Counter extends Component {
+  static propTypes = {
+    increment: PropTypes.func.isRequired,
+    decrement: PropTypes.func.isRequired,
+    incrementIfOdd: PropTypes.func.isRequired,
+    incrementAsync: PropTypes.func.isRequired,
+    load: PropTypes.func.isRequired,
+    state: PropTypes.object.isRequired
   };
+
+  render() {
+    const {
+      increment, incrementAsync, decrement, incrementIfOdd, counter
+    } = this.props;
+
+    return (
+      <p>
+        clicked {counter} times:
+        {'  '}
+        <button onClick={increment}>+</button>
+        {'  '}
+        <button onClick={decrement}>-</button>
+        {'  '}
+        <button onClick={incrementIfOdd}>increment if odd</button>
+        {'  '}
+        <button onClick={() => incrementAsync()}>increment Async</button>
+        {'   '}
+        <button onClick={load}>load</button>
+        <br />
+        程序当前的state:
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      </p>
+    )
+  }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(CounterActions, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
