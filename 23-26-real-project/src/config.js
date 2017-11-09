8916997562e2817f49c8bc75bd96b122c495e204
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-09 10:23:10 +0800

    配置变量

---
+++ 23-26-real-project/src/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || (process.env.NODE_ENV === 'production' ? 8080 : 3000),
+  apiHost: process.env.APIHOST || 'localhost',
+  apiPort: process.env.APIHOST || 3030,
+  app: {
+    title: 'react redux book',
+    description: 'react redux book example',
+    head: {
+      titleTemplate: 'react redux book:%s',
+      meta: [
+        {
+          name: 'description',
+          content: 'react redux book example',
+        },
+        {
+          charset: 'utf-8',
+        },
+      ],
+    },
+  },
+}
+;
