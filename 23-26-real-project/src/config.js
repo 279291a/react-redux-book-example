3fc473fbdeb752c49e866d112418f3616cb36478
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-09 11:51:32 +0800

    设置apiPort

--- 23-26-real-project/src/config.js
+++ 23-26-real-project/src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || (process.env.NODE_ENV === 'production' ? 8080 : 3000),
   apiHost: process.env.APIHOST || 'localhost',
-  apiPort: process.env.APIHOST || 3030,
+  apiPort: process.env.APIPORT || 3030,
   app: {
     title: 'react redux book',
     description: 'react redux book example',
@@ -19,5 +19,4 @@ module.exports = {
       ],
     },
   },
-}
-;
+};
