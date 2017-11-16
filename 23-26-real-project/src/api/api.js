e05f33c6cfe7c3610d68a9db88bfdfde0b2535ff
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-16 13:42:11 +0800

    配置express-session中间

--- 23-26-real-project/src/api/api.js
+++ 23-26-real-project/src/api/api.js
@@ -1,9 +1,19 @@
 import Express from 'express';
+import bodyParser from 'body-parser';
+import session from 'express-session';
 import config from '../config';
 import controllers from './controllers';
 const port = config.apiPort;
 const app = new Express();
 
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'react redux book!!!',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 },
+}));
 controllers(app);
 
 app.listen(port, (error) => {
@@ -12,4 +22,4 @@ app.listen(port, (error) => {
   } else {
     console.info('==> api listening on port %s.', port);
   }
-})
+});
