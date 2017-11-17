40854b2582ed6306d157fecc62a29208fddaed17
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-17 13:36:37 +0800

    综合导出所有容器组件

---
+++ 23-26-real-project/src/containers/index.js
@@ -0,0 +1,13 @@
+import Counter from './Counter/Counter';
+import Form from './Forms/Form';
+import Login from './Login/Login';
+import Main from './Main/Main';
+import Spin from './Spin/Spin';
+
+export default {
+  Counter,
+  Form,
+  Login,
+  Main,
+  Spin,
+};
