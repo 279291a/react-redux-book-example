db9ac492850401b6078935fd8cf5b9b1c14b740e
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-17 13:33:56 +0800

    将所有展示组件综合导出

---
+++ 23-26-real-project/src/components/index.js
@@ -0,0 +1,17 @@
+import Spin from './Spin/Spin';
+import SimpleForm from './ReduxForm/SimpleForm';
+import SynchronousValidationForm from './ReduxForm/SynchronousValidationsForm';
+import AsynchronousBlurValidationForm from './ReduxForm/AsynchronousBlurValidationForm';
+import Line from './Charts/Line';
+import Column from './Charts/Column';
+import Table from './Table/Table';
+
+export default {
+  Spin,
+  SimpleForm,
+  SynchronousValidationForm,
+  AsynchronousBlurValidationForm,
+  Line,
+  Column,
+  Table,
+};
