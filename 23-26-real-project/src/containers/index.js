b34693260fc6c568a86fb65d1ec70cb4c042082a
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-17 14:07:59 +0800

    增加组件导出

--- 23-26-real-project/src/containers/index.js
+++ 23-26-real-project/src/containers/index.js
@@ -1,6 +1,9 @@
 import Counter from './Counter/Counter';
 import Form from './Forms/Form';
 import Login from './Login/Login';
+import Home from './Home/Home';
+import NotFound from './NotFound/NotFound';
+import Statistic from './Statistic/Statistic';
 import Main from './Main/Main';
 import Spin from './Spin/Spin';
 
@@ -10,4 +13,7 @@ export default {
   Login,
   Main,
   Spin,
+  Home,
+  NotFound,
+  Statistic,
 };
