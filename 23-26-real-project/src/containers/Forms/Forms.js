2c115e72d3cf318e6f3aabdc0ff0cb89db7b0fd9
王晓丽 <xiaoli.wang@echoesnet.com>
2017-11-20 15:20:31 +0800

    修正表单不能显示的bug

--- 23-26-real-project/src/containers/Forms/Forms.js
+++ 23-26-real-project/src/containers/Forms/Forms.js
@@ -4,9 +4,13 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import serialize from 'serialize-javascript';
 import * as ActionCreators from '../../actions/forms';
-import { SimpleForm, SynchronousValidattionForm, AsynchronousBlurValidationForm } from '../../components/'
+// import AsynchronousBlurValidationForm from "../../components/ReduxForm/AsynchronousBlurValidationForm";
+// import SimpleForm from "../../components/ReduxForm/SimpleForm";
+// import SynchronousValidationsForm from "../../components/ReduxForm/SynchronousValidationsForm";
+import { AsynchronousBlurValidationForm, SimpleForm, SynchronousValidationsForm } from '../../components';
 
-@connect(
+// eslint-disable-next-line
+@connect(//eslint-disable-line
   state => ({
     form: state.async.forms,
     formState: state.async.loadState && state.async.loadState.forms
@@ -26,39 +30,38 @@ class Forms extends Component {
     const { form, formState, post } = this.props;
 
     return (
-      // <div className={styles.forms}>
-      //   <Helme title="表单" />
-      //   <Grid>
-      //     <Row>
-      //       <Col xs={12} md={4}>
-      //         <h4>SimpleForm</h4>
-      //         <SimpleForm onSubmit={post} />
-      //       </Col>
-      //       <Col xs={12} md={8}>
-      //         <Row>
-      //           <Col xs={12} md={6}>
-      //             <h4>SynchronousValidattionForm</h4>
-      //             <SynchronousValidattionForm onSubmit={post} />
-      //           </Col>
+      <div className={styles.forms}>
+        <Helmet title="表单" />
+        <Grid>
+          <Row>
+            <Col xs={12} md={4}>
+              <h4>SimpleForm</h4>
+              <SimpleForm onSubmit={post} />
+            </Col>
+            <Col xs={12} md={8}>
+              <Row>
+                <Col xs={12} md={6}>
+                  <h4>SynchronousValidattionForm</h4>
+                  <SynchronousValidationsForm onSubmit={post} />
+                </Col>
 
-      //           <Col xs={12} md={6}>
-      //             <h4>AsynchronousBlurValidationForm</h4>
-      //             <AsynchronousBlurValidationForm onSubmit={post} />
-      //           </Col>
-      //         </Row>
+                <Col xs={12} md={6}>
+                  <h4>AsynchronousBlurValidationForm</h4>
+                  <AsynchronousBlurValidationForm onSubmit={post} />
+                </Col>
+              </Row>
 
-      //         <br /><br />
+              <br /><br />
 
-      //         <Row className="container-fluid">
-      //           表单提交后的响应结果:
-      //           <pre>{serialize(forms, { space: 2 })}</pre>
-      //           <pre>{serialize(formState, { space: 2 })}</pre>
-      //         </Row>
-      //       </Col>
-      //     </Row>
-      //   </Grid>
-      // </div >
-      <p>biaodan </p>
+              <Row className="container-fluid">
+                表单提交后的响应结果:
+                <pre>{serialize(form, { space: 2 })}</pre>
+                <pre>{serialize(formState, { space: 2 })}</pre>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
+      </div >
     )
   }
 }
